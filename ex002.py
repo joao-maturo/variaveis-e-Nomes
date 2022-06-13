82ff9166197eeaa25a5b9c86ac3f7dbddaac0b94
João Pedro Maturo Sofalo <102874668+joao-maturo@users.noreply.github.com>
2022-06-13 13:11:07 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,17 @@
+# variaveis e Nomes
+carros = '100'
+espaço_no_carro = '4.0M'
+dirigiveis = '30'
+passageiros = '90'
+carros_não_dirigiveis = 'carros não dirigiveis'
+carros_dirigiveis = 'dirigiveis'
+motor = 'v8'
+
+
+print(carros)
+print(espaço_no_carro)
+print(dirigiveis)
+print(passageiros)
+print(carros_não_dirigiveis)
+print(carros_dirigiveis)
+print(motor)
